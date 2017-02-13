0639be683fbd7aaee8cc7f34658291dffbbfc673
Laura Zodrow <laura.zodrow@gmail.com>
2017-02-12 17:19:16 -0700

    first commit

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const serveStatic = require('serve-static')
 const routes = require('./api/routes')
 // const mongoose = require('mongoose');
 // const ArticleData = require('./models/articleData');
-// const config = require('./config')
 const sockets = require('./api/socket')
 const Twitter = require('twit')
+
+const twitter = new Twitter({
+  consumer_key: "BSN8JOEEDLCB4EOgSJT8hTWWO",
+  consumer_secret: "H9j6bCuUl48CpoVOc4MNnxgpdnGrhc7gaMzTePnAnKcSqDr80u",
+  access_token: "468388067-VkXEzmvpdXoVpOYgeXrq1VcHAWDyDQKvKbYVw5fQ",
+  access_token_secret: "cgrmisBMM6yhQiq3tRWBuGzRoLRk7NfeICKShgYbY5yFa",
+  timeout_ms: 60*1000, 
+})
 
 
 const app = express();
@@ -29,7 +36,7 @@ const cors = CORS({
     credentials: true
 })
 
-// mongoose.connect(config.database)
+// mongoose.connect(process.env.MONGO_URI || 'localhost/net-gather')
 // mongoose.connection.on('error', function() {
 //   console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
 // });
@@ -50,14 +57,6 @@ if (process.env.NODE_ENV === 'production') {
 const server = app.listen(process.env.PORT || 3001, () => {
     console.log("API listening at PORT:" + process.env.PORT);
 });
-
-const twitter = new Twitter({
-  consumer_key: "BSN8JOEEDLCB4EOgSJT8hTWWO",
-  consumer_secret: "H9j6bCuUl48CpoVOc4MNnxgpdnGrhc7gaMzTePnAnKcSqDr80u",
-  access_token: "468388067-VkXEzmvpdXoVpOYgeXrq1VcHAWDyDQKvKbYVw5fQ",
-  access_token_secret: "cgrmisBMM6yhQiq3tRWBuGzRoLRk7NfeICKShgYbY5yFa",
-  timeout_ms: 60*1000, 
-})
 
 const io = require('socket.io').listen(server);
 
